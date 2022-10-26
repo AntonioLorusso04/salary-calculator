b71c187fa3865bbd9ebf53b29d05b1ba95ed9226
AntonioLorusso04 <lorusso.antonio@panettipitagora.edu.it>
2022-10-27 00:41:39 +0200

    modifiche php

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
 <?php
+if (isset($_POST["calcola"])) 
+{
+	$giorni = $_POST["giorni"];
+	$ore_giornaliere = $_POST["ore_giornaliere"];
+	$compenso_orario = $_POST["compenso_orario"];
+
+    $guadagno = $giorni*$ore_giornaliere*$compenso_orario;
+
+    if ($ore_giornaliere == 0 || $compenso_orario == 0)
+    {
+    	echo "<script> alert('Attenzione, inserire un numero maggiore di 0!'); </script>";
+    }
+
+    else
+    {
+    	echo "Guadagnerai $guadagno€ lavorando $giorni giorni al compenso di $compenso_orario€/h";
+    }
+}
