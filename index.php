c6f37e5738d179d0b4d37821fa6b189eabb79e49
AntonioLorusso04 <lorusso.antonio@panettipitagora.edu.it>
2022-10-28 00:36:04 +0200

    prime modifiche html

--- index.php
+++ index.php
@@ -16,4 +16,48 @@ if (isset($_POST["calcola"]))
     {
     	echo "Guadagnerai $guadagno€ lavorando $giorni giorni al compenso di $compenso_orario€/h";
     }
-}
+} 
+
+?>
+
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="static/css/style.css">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+</head>
+<body class="background">
+	<div class="container">
+		<div class="row">
+			<div class="col-12">
+				<p class="title"><b>Quanto sarai povero?</b></p>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-12">
+				<p class="paragraph"><b>Giorni</b></p>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-12">
+				<input type="text" name="durata_lavoro" class="text_box" placeholder="Quanti giorni durerà il lavoro?">
+			</div>
+
+		<div class="row">
+			<div class="col-12">
+				<hr class="linea">
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-12">
+				<p class="campo_obbligatorio"><b>Campo obbligatorio</b></p>
+			</div>
+		</div>
+
+
+
+</body>
+</html>
